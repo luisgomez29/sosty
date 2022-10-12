57dcc6c47ff9ac2b7d237cb957fcc174fdf320fb
ortegabrro <ortegabrro@gmail.com>
2022-10-11 20:11:04 -0500

    form validations

--- lib/ui/common/validations/validation_messages.dart
+++ lib/ui/common/validations/validation_messages.dart
@@ -4,4 +4,9 @@ class ValidationMessages {
   static String passwordRequired = "La constraseña es requerida";
   static String passwordMinLengthInvalid = "La constraseña es muy corta";
   static String passwordMaxLengthInvalid = "La constraseña es muy larga";
+  static String nameRequired = "El nombre es requerido";
+  static String lastnameRequired = "El apellido es requerido";
+  static String cellphoneRequired = "El celular es requerido";
+  static String fieldMinLength = "Valor minimo 3 caracteres";
+  static String cellphoneInvalid = "El celular no es valido";
 }
