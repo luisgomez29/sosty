6f195b145812816e14b11f1120d03ae6fe16e693
Mauricio Quijano <mauricioquijano@gmail.com>
2022-10-25 12:14:33 -0500

    add foreground and background push notifications

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,8 @@
-import 'package:flutter/foundation.dart';
+import 'package:sosty/push_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:sosty/app.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 
-Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
-  // If you're going to use other Firebase services in the background, such as Firestore,
-  // make sure you call `initializeApp` before using other Firebase services.
-  await Firebase.initializeApp();
-
-  if (kDebugMode) {
-    print("Handling a background message: ${message.messageId}");
-  }
-}
-
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  FirebaseMessaging messaging = FirebaseMessaging.instance;
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
-
-  NotificationSettings settings = await messaging.requestPermission(
-    alert: true,
-    announcement: false,
-    badge: true,
-    carPlay: false,
-    criticalAlert: false,
-    provisional: false,
-    sound: true,
-  );
-
-  if (kDebugMode) {
-    print('User granted permission: ${settings.authorizationStatus}');
-  }
-
-  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-    if (kDebugMode) {
-      print('Got a message whilst in the foreground!');
-      print('Message data: ${message.data}');
-
-      if (message.notification != null) {
-        print('Message also contained a notification: ${message.notification}');
-      }
-    }
-  });
-
+Future<void> main() async {
+  await Notifications.init();
   runApp(const App());
 }
